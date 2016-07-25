c2a6b372ca443ad64ee061225852affe78966812
theSC <troy.lundgren@gmail.com>
2016-07-25 01:09:17 -0700

    Update raspicoolermon.py

--- raspicoolermon.py
+++ raspicoolermon.py
@@ -1,25 +1,5 @@
 #!/usr/bin/python
-#--------------------------------------
 
-# The wiring for the LCD is as follows:
-# 1 : GND
-# 2 : 5V
-# 3 : Contrast (0-5V)*
-# 4 : RS (Register Select)
-# 5 : R/W (Read Write)       - GROUND THIS PIN
-# 6 : Enable or Strobe
-# 7 : Data Bit 0             - NOT USED
-# 8 : Data Bit 1             - NOT USED
-# 9 : Data Bit 2             - NOT USED
-# 10: Data Bit 3             - NOT USED
-# 11: Data Bit 4
-# 12: Data Bit 5
-# 13: Data Bit 6
-# 14: Data Bit 7
-# 15: LCD Backlight +5V**
-# 16: LCD Backlight GND
-
-#import
 import RPi.GPIO as GPIO
 import time
 import netifaces as ni
@@ -30,7 +10,6 @@ import logging
 import feedparser
 import subprocess
 from picamera import PiCamera
-#from gpiozero import Button
 
 camera = PiCamera()
 GPIO.setmode(GPIO.BCM)
@@ -60,6 +39,9 @@ E_DELAY = 0.0005
 screen_delay = 5
 
 rssurl = 'YOUR RSS FEED HERE'
+owmapi = 'YOUR OWM API KEY HERE'
+location = 'YOUR LOCATION HERE ex Vancouver, BC'
+
 logger = logging.getLogger('raspicoolermon')
 hdlr = logging.FileHandler('logs/raspicoolermon.log')
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
@@ -68,7 +50,7 @@ logger.addHandler(hdlr)
 logger.setLevel(logging.INFO)
 
 
-def frontdooropen(channel):
+def dooropen(channel):
     timenow = time.strftime('%H:%M:%S')
     print "%s Door opened" % timenow
     subprocess.call("./pushbullet 'Door' 'opened @ %s'" % timenow, shell=True)
@@ -91,10 +73,7 @@ def main():
 
   ip = ni.ifaddresses('wlan0')[2][0]['addr']
   
-  #button.wait_for_press(0)
-  #camera.capture('/home/pi/test_image.jpg')
-
-  # Initialise display
+    # Initialise display
   lcd_init()
   lcd_string("Hello!",LCD_LINE_1)
   lcd_string("Setting up...",LCD_LINE_2)
@@ -117,13 +96,13 @@ def main():
   time.sleep(5)
   print("Entering main loop")
   print("Press: Ctrl+C to exit...")
-  GPIO.add_event_detect(17, GPIO.FALLING, callback=frontdooropen)
+  GPIO.add_event_detect(17, GPIO.FALLING, callback=dooropen)
   while True:
     try:
       timeshort = time.strftime('%H:%M')
       currenttime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-      owm = pyowm.OWM('YOUR OWM API')
-      observation = owm.weather_at_place('YOUR LOCATION)
+      owm = pyowm.OWM('%s' % owmapi)
+      observation = owm.weather_at_place('%s' % location)
       w = observation.get_weather()
       ct = w.get_temperature(unit='celsius')
       ctemp = re.search(r"(?<='temp': ).*?(?=, 'temp_min)", '%s' % ct).group(0)
@@ -169,34 +148,24 @@ def main():
          lcd_text = entry1_long_string[i:(i+15)]
          lcd_string(lcd_text,LCD_LINE_2)
          time.sleep(0.2)
-        #lcd_byte(LCD_LINE_2, LCD_CMD)
-        #lcd_string(str_pad,1)
-    
-        #time.sleep(1)
 
         entry2 = feed['entries'][1]['title']
         entry2_long_string = entry2
         entry2_long_string = str_pad + entry2_long_string
-        #lcd_string("%s" % feed_title, LCD_LINE_1)
         for i in range (0, len(entry2_long_string)):
          lcd_byte(LCD_LINE_1, LCD_CMD)
          lcd_text = entry2_long_string[i:(i+15)]
          lcd_string(lcd_text,LCD_LINE_2)
          time.sleep(0.2)
-        #lcd_byte(LCD_LINE_2, LCD_CMD)
-        #lcd_string(str_pad,1)
 
         entry3 = feed['entries'][2]['title']
         entry3_long_string = entry3
         entry3_long_string = str_pad + entry3_long_string
-        #lcd_string("%s" % feed_title, LCD_LINE_1)
         for i in range (0, len(entry3_long_string)):
          lcd_byte(LCD_LINE_1, LCD_CMD)
          lcd_text = entry3_long_string[i:(i+15)]
          lcd_string(lcd_text,LCD_LINE_2)
          time.sleep(0.2)
-        #lcd_byte(LCD_LINE_2, LCD_CMD)
-        #lcd_string(str_pad,1)
 
       except Exception as ex:
         template = "An exception of type {0} occured. Arguments:\n{1!r}"
